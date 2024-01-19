6ffc9e23eab37444b899cffb066861ae32040c0f
bright <bright2kwame@gmail.com>
2024-01-19 12:57:33 +0000

    pushed the code regards to drawer

--- lib/music_home_page.dart
+++ lib/music_home_page.dart
@@ -20,46 +20,81 @@ class _MusicHomePageState extends State<MusicHomePage> {
     SettingsPage()
   ];
 
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+  final AppBar _topAppBar = AppBar(
+    elevation: 4,
+    backgroundColor: Colors.black,
+    leadingWidth: 150,
+    leading: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Text(
+        "Tracks",
+        style: TextStyle(
+            color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldState,
       backgroundColor: Colors.black,
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.credit_card), label: "Payments"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: "Notifications"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
-        ],
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.black,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        onTap: (index) {
-          setState(() {
-            activeIndex = index;
-          });
-        },
-        currentIndex: activeIndex,
-      ),
-      appBar: AppBar(
-        elevation: 4,
-        backgroundColor: Colors.black,
-        leadingWidth: 150,
-        leading: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8),
-          child: Text(
-            "Tracks",
-            style: TextStyle(
-                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
+      drawer: _myAppDrawer(),
+      bottomNavigationBar: _myAppBar(),
+      appBar: _topAppBar,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton:
+          ElevatedButton(onPressed: _openAppDrawer, child: Text("Open Menu")),
       body: pages[activeIndex],
       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _myAppBar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card), label: "Payments"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.notifications), label: "Notifications"),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
+      ],
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.black,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      onTap: (index) {
+        setState(() {
+          activeIndex = index;
+        });
+      },
+      currentIndex: activeIndex,
+    );
+  }
+
+  void _openAppDrawer() {
+    _scaffoldState.currentState!.openDrawer();
+  }
+
+  Widget _myAppDrawer() {
+    return Drawer(
+      backgroundColor: Colors.white,
+      width: 240,
+      child: Column(
+        children: [
+          DrawerHeader(
+              child: ListTile(
+            onTap: () {
+              print("Action on");
+              Navigator.of(context).pop();
+            },
+            leading: Icon(Icons.person),
+          ))
+        ],
+      ),
     );
   }
 }
